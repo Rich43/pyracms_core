6c4bef9fff7e724adbc10e6a2bf1ecc9a26e92a9
Richie Ward <RichieS@GMail.com>
2015-05-12 14:02:48 +0100

    Fix deform template urls and slight change to __init__ to make it all gel together :)

--- pyracms/__init__.py
+++ pyracms/__init__.py
@@ -42,8 +42,7 @@ def main(global_config, **settings):
     config.add_subscriber(add_renderer_globals, BeforeRender)
     config.add_static_view('static', settings.get('static_path'), 
                            cache_max_age=3600)
-    config.add_static_view("dstatic", "deform:static", 
-                           cache_max_age=3600)
+    config.add_static_view('static-deform', 'deform:static')
     if settings.get("enable_pyracms_home"):
         config.add_view(dummy_home, route_name="home", 
                         renderer="dummy_home.jinja2")
